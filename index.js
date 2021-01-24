e2625aeed17712964850b8575183a1d4172eed8e
Josh Bryson <josh@bryson.me.uk>
2021-01-24 21:41:26 +0000

    Tray icon display toggle added

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+const path = require('path');
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
         height: 500,
         width: 300,
         frame: false,
         resizable: false,
-        webPreferences: {
-            nodeIntegration: true
+        show: false
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
+    tray = new Tray(iconPath);
+    tray.on('click', () => {
+        if(mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            mainWindow.show();
         }
-    })
-    mainWindow.locaURL(`file://${__dirname}/src/index.html`)
+    });
 });
